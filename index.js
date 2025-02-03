eee57514dabad9366263dc43710910ac1125b808
iUnknown                    ث <147574769+iUnknownBr@users.noreply.github.com>
2025-02-03 19:55:48 -0300

    Update index.js

--- index.js
+++ index.js
@@ -373,7 +373,7 @@ function setupMain(){
         }, 3000);
     }
     async function autoAnswer() {
-        const baseClasses = ["_1r8cd7xe", "_ssxvf9l", "_ek84n5e", "_rz7ls7u", "_1yok8f4", "_1e5cuk2a"];
+        const baseClasses = ["_1r8cd7xe", "_ssxvf9l", "_19uopuu", "_rz7ls7u", "_1yok8f4", "_1e5cuk2a"];
         while (true) {
             if(features.autoAnswer&&features.questionSpoof){
                 const classToCheck = [...baseClasses];
