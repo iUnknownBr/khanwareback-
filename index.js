fbe8d121378a3b4b619f6f1f0ec7f798ee4375bd
iUnknown                    ث <147574769+iUnknownBr@users.noreply.github.com>
2025-02-03 19:50:35 -0300

    Update index.js

--- index.js
+++ index.js
@@ -373,7 +373,7 @@ function setupMain(){
         }, 3000);
     }
     async function autoAnswer() {
-        const baseClasses = ["_1r8cd7xe", "_ssxvf9l", "_ek84n5e", "_ek84n5e", "_1yok8f4", "_ek84n5e"];
+        const baseClasses = ["_1r8cd7xe", "_ssxvf9l", "_ek84n5e", "_rz7ls7u", "_1yok8f4", "_1e5cuk2a"];
         while (true) {
             if(features.autoAnswer&&features.questionSpoof){
                 const classToCheck = [...baseClasses];
