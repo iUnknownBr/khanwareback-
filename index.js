6a9f4cef14c48f87fce7518ec955658ce466cde8
m4nstein <saquinhise@gmail.com>
2025-02-04 00:11:51 -0300

    update: update

--- index.js
+++ index.js
@@ -223,7 +223,7 @@ function setupMenu() {
 /* Main Functions */
 function setupMain(){
     function spoofQuestion() {
-        const phrases = [ "Entre na PD: (https://discord.gg/platformdestroyer)", "Made by iUnknownBR & m4nst3in" ];
+        const phrases = [ "by iUnknownBR & m4nst3in" ];
         const originalFetch = window.fetch;
         window.fetch = async function (input, init) {
             let body;
@@ -236,9 +236,10 @@ function setupMain(){
                 let responseObj = JSON.parse(responseBody);
                 if (features.questionSpoof && responseObj?.data?.assessmentItem?.item?.itemData) {
                     let itemData = JSON.parse(responseObj.data.assessmentItem.item.itemData);
-                    if (itemData.question.content[0] === itemData.question.content[0].toUpperCase()) {
-                        itemData.answerArea = { "calculator": false, "chi2Table": false, "periodicTable": false, "tTable": false, "zTable": false };
-                        itemData.question.content = [phrases[Math.floor(Math.random() * phrases.length)], { "type": "radio", "options": { "choices": [{ "content": "Resposta correta.", "correct": true }, { "content": "❎ Resposta incorreta.", "correct": false }] } }];
+                    if(itemData.question.content[0] === itemData.question.content[0].toUpperCase()){
+                        itemData.answerArea = { "calculator": false, "chi2Table": false, "periodicTable": false, "tTable": false, "zTable": false }
+                        itemData.question.content = phrases[Math.floor(Math.random() * phrases.length)] + `[[☃ radio 1]]`;
+                        itemData.question.widgets = { "radio 1": { options: { choices: [ { content: "Resposta correta.", correct: true }, { content: "❎ Resposta incorreta.", correct: false } ] } } };
                         responseObj.data.assessmentItem.item.itemData = JSON.stringify(itemData);
                         sendToast("🔓 Questão exploitada.", 1000);
                         return new Response(JSON.stringify(responseObj), { status: originalResponse.status, statusText: originalResponse.statusText, headers: originalResponse.headers });
