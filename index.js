588e5bb58de206dd8b6f6de8aba824e02e0f9e60
m4nstein <saquinhise@gmail.com>
2025-02-04 00:07:23 -0300

    fix: remove radio

--- index.js
+++ index.js
@@ -236,10 +236,9 @@ function setupMain(){
                 let responseObj = JSON.parse(responseBody);
                 if (features.questionSpoof && responseObj?.data?.assessmentItem?.item?.itemData) {
                     let itemData = JSON.parse(responseObj.data.assessmentItem.item.itemData);
-                    if(itemData.question.content[0] === itemData.question.content[0].toUpperCase()){
-                        itemData.answerArea = { "calculator": false, "chi2Table": false, "periodicTable": false, "tTable": false, "zTable": false }
-                        itemData.question.content = phrases[Math.floor(Math.random() * phrases.length)] + `[[☃ radio 1]]`;
-                        itemData.question.widgets = { "radio 1": { options: { choices: [ { content: "Resposta correta.", correct: true }, { content: "❎ Resposta incorreta.", correct: false } ] } } };
+                    if (itemData.question.content[0] === itemData.question.content[0].toUpperCase()) {
+                        itemData.answerArea = { "calculator": false, "chi2Table": false, "periodicTable": false, "tTable": false, "zTable": false };
+                        itemData.question.content = [phrases[Math.floor(Math.random() * phrases.length)], { "type": "radio", "options": { "choices": [{ "content": "Resposta correta.", "correct": true }, { "content": "❎ Resposta incorreta.", "correct": false }] } }];
                         responseObj.data.assessmentItem.item.itemData = JSON.stringify(itemData);
                         sendToast("🔓 Questão exploitada.", 1000);
                         return new Response(JSON.stringify(responseObj), { status: originalResponse.status, statusText: originalResponse.statusText, headers: originalResponse.headers });
