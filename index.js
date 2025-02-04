35fc767cdc036a475aa2b460ffb0ccbf734423c5
m4nstein <saquinhise@gmail.com>
2025-02-03 23:34:42 -0300

    revert: hud sht not working

--- index.js
+++ index.js
@@ -135,171 +135,47 @@ function setupMenu() {
             cursor: 'default', userSelect: 'none', padding: '0 10px',  borderRadius: '10px', zIndex: '1001', transition: 'transform 0.3s ease'
         });
         if (device.mobile) watermark.style.left = '55%'
-        watermark.innerHTML = `<span style="color: #39FF14;">kD</span> <span style="color:gray; padding-left:2px; font-family: Arial, sans-serif; font-size:10px">${ver}</span>`;        document.body.appendChild(watermark);
+        watermark.innerHTML = `<span style="text-shadow: -1px 0.5px 0 #72ff72, -2px 0px 0 #2f672e;">kd</span> <span style="color:gray; padding-left:2px; font-family: Arial, sans-serif; font-size:10px">${ver}</span>`;
+        document.body.appendChild(watermark);
         let isDragging = false, offsetX, offsetY;
         watermark.addEventListener('mousedown', e => { if (!dropdownMenu.contains(e.target)) { isDragging = true; offsetX = e.clientX - watermark.offsetLeft; offsetY = e.clientY - watermark.offsetTop; watermark.style.transform = 'scale(0.9)'; unloader.style.transform = 'scale(1)'; } });
         watermark.addEventListener('mouseup', () => { isDragging = false; watermark.style.transform = 'scale(1)'; unloader.style.transform = 'scale(0)'; if (checkCollision(watermark.getBoundingClientRect(), unloader.getBoundingClientRect())) unload(); });
         document.addEventListener('mousemove', e => { if (isDragging) { let newX = Math.max(0, Math.min(e.clientX - offsetX, window.innerWidth - watermark.offsetWidth)); let newY = Math.max(0, Math.min(e.clientY - offsetY, window.innerHeight - watermark.offsetHeight)); Object.assign(watermark.style, { left: `${newX}px`, top: `${newY}px` }); dropdownMenu.style.display = 'none'; } });
     }
-
-    function openOptionsWindow() {
-        const optionsWindow = document.createElement('div');
-        Object.assign(optionsWindow.style, {
-            position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)',
-            width: '300px', height: '400px', backgroundColor: 'rgba(0,0,0,0.8)', color: 'white',
-            borderRadius: '10px', zIndex: '1001', padding: '20px', display: 'flex', flexDirection: 'column',
-            justifyContent: 'center', alignItems: 'center', overflowY: 'auto'
+    function setupDropdown() {
+        Object.assign(dropdownMenu.style, {
+            position: 'absolute', top: '100%', left: '0', width: '160px', backgroundColor: 'rgba(0,0,0,0.3)',
+            borderRadius: '10px', color: 'white', fontSize: '13px', fontFamily: 'Monospace, sans-serif',
+            display: 'none', flexDirection: 'column', zIndex: '1000', padding: '5px', cursor: 'default',
+            userSelect: 'none', transition: 'transform 0.3s ease', backdropFilter: 'blur(2px)', WebkitBackdropFilter: 'blur(2px)'
         });
-
-        // Add title
-        const title = document.createElement('div');
-        title.innerHTML = `<span style="color: white;">Khan</span><span style="color: #39FF14;">Destroyer</span>`;
-        Object.assign(title.style, {
-            fontSize: '24px', fontWeight: 'bold', marginBottom: '20px', textAlign: 'center'
-        });
-        optionsWindow.appendChild(title);
-
-        // Add features list and input handling
-        optionsWindow.innerHTML += `
-        <style>
-            input[type="checkbox"] {appearance: none; width: 15px; height: 15px; background-color: #3a3a3b;
-            border: 1px solid #acacac; border-radius: 3px; margin-right: 5px; cursor: pointer;}
-            input[type="checkbox"]:checked {background-color: #540b8a; border-color: #720fb8;}
-            input[type="text"], input[type="number"], input[type="range"] {width: calc(100% - 10px); border: 1px solid #343434;
-            color: white; accent-color: #540b8a; background-color: #540b8a; padding: 3px; border-radius: 3px; background: none;}
-            label {display: flex; align-items: center; color: #3a3a3b; padding-top: 3px;}
-        </style>
-    `;
-
+        dropdownMenu.innerHTML = `
+            <style>
+                input[type="checkbox"] {appearance: none; width: 15px; height: 15px; background-color: #3a3a3b;
+                border: 1px solid #acacac; border-radius: 3px; margin-right: 5px; cursor: pointer;}
+                input[type="checkbox"]:checked {background-color: #540b8a; border-color: #720fb8;}
+                input[type="text"], input[type="number"], input[type="range"] {width: calc(100% - 10px); border: 1px solid #343434;
+                color: white; accent-color: #540b8a; background-color: #540b8a; padding: 3px; border-radius: 3px; background: none;}
+                label {display: flex; align-items: center; color: #3a3a3b; padding-top: 3px;}
+            </style>
+        `;
+        watermark.appendChild(dropdownMenu);
         let featuresList = [
-            [{ name: 'questionSpoof', type: 'checkbox', variable: 'features.questionSpoof', labeled: true, label: 'Question Spoof' },
-                { name: 'videoSpoof', type: 'checkbox', variable: 'features.videoSpoof', labeled: true, label: 'Video Spoof' },
+            [{ name: 'questionSpoof', type: 'checkbox', variable: 'features.questionSpoof', attributes: 'checked', labeled: true, label: 'Question Spoof' },
+                { name: 'videoSpoof', type: 'checkbox', variable: 'features.videoSpoof', attributes: 'checked', labeled: true, label: 'Video Spoof' },
                 { name: 'showAnswers', type: 'checkbox', variable: 'features.showAnswers', labeled: true, label: 'Answer Revealer' }],
             [{ name: 'autoAnswer', type: 'checkbox', variable: 'features.autoAnswer', dependent: 'autoAnswerDelay,nextRecomendation,repeatQuestion', labeled: true, label: 'Auto Answer' },
                 { name: 'repeatQuestion', className: 'repeatQuestion', type: 'checkbox', variable: 'features.repeatQuestion', attributes: 'style="display:none;"', labeled: true, label: 'Repeat Question' },
                 { name: 'nextRecomendation', className: 'nextRecomendation', type: 'checkbox', variable: 'features.nextRecomendation', attributes: 'style="display:none;"', labeled: true, label: 'Recomendations' },
-                { name: 'autoAnswerDelay', className: 'autoAnswerDelay', type: 'range', variable: 'featureConfigs.autoAnswerDelay', attributes: 'style="display:none;" min="1" max="3" value="1"', labeled: false }],
-        ];
-
-        function addFeature(features) {
-            features.forEach(elements => {
-                const feature = document.createElement('feature');
-                elements.forEach(attribute => {
-                    let element = attribute.type === 'nonInput' ? document.createElement('label') : document.createElement('input');
-                    if (attribute.type === 'nonInput') element.innerHTML = attribute.name;
-                    else { element.type = attribute.type; element.id = attribute.name; }
-
-                    if (attribute.attributes) {
-                        attribute.attributes.split(' ').map(attr => attr.split('=')).forEach(([key, value]) => {
-                            value = value ? value.replace(/"/g, '') : '';
-                            key === 'style' ? element.style.cssText = value : element.setAttribute(key, value);
-                        });
-                    }
-
-                    if (attribute.variable) element.setAttribute('setting-data', attribute.variable);
-                    if (attribute.dependent) element.setAttribute('dependent', attribute.dependent);
-                    if (attribute.className) element.classList.add(attribute.className);
-
-                    if (attribute.labeled) {
-                        const label = document.createElement('label');
-                        if (attribute.className) label.classList.add(attribute.className);
-                        if (attribute.attributes) {
-                            attribute.attributes.split(' ').map(attr => attr.split('=')).forEach(([key, value]) => {
-                                value = value ? value.replace(/"/g, '') : '';
-                                key === 'style' ? label.style.cssText = value : label.setAttribute(key, value);
-                            });
-                        }
-                        label.innerHTML = `${element.outerHTML} ${attribute.label}`;
-                        feature.appendChild(label);
-                    } else {
-                        feature.appendChild(element);
-                    }
-                });
-                optionsWindow.innerHTML += feature.outerHTML;
-            });
-        }
-
-        function handleInput(ids, callback = null) {
-            (Array.isArray(ids) ? ids.map(id => document.getElementById(id)) : [document.getElementById(ids)])
-                .forEach(element => {
-                    if (!element) return;
-                    const setting = element.getAttribute('setting-data'),
-                        dependent = element.getAttribute('dependent'),
-                        handleEvent = (e, value) => {
-                            setFeatureByPath(setting, value);
-                            if (callback) callback(value, e);
-                        };
-
-                    if (element.type === 'checkbox') {
-                        element.checked = getFeatureByPath(setting);
-                        element.addEventListener('change', (e) => {
-                            playAudio('https://r2.e-z.host/4d0a0bea-60f8-44d6-9e74-3032a64a9f32/5os0bypi.wav');
-                            handleEvent(e, e.target.checked);
-                            if (dependent) dependent.split(',').forEach(dep =>
-                                document.querySelectorAll(`.${dep}`).forEach(depEl =>
-                                    depEl.style.display = e.target.checked ? null : "none"));
-                        });
-                    } else {
-                        element.value = getFeatureByPath(setting);
-                        element.addEventListener('input', (e) => handleEvent(e, e.target.value));
-                    }
-                });
-        }
-
-        function getFeatureByPath(path) {
-            let obj = window;
-            const parts = path.split('.');
-            while (parts.length > 1) obj = obj[parts.shift()];
-            return obj[parts[0]];
-        }
-
-        function setFeatureByPath(path, value) {
-            let obj = window;
-            const parts = path.split('.');
-            while (parts.length > 1) obj = obj[parts.shift()];
-            obj[parts[0]] = value;
-        }
+                { name: 'autoAnswerDelay', className: 'autoAnswerDelay', type: 'range', variable: 'featureConfigs.autoAnswerDelay', attributes: 'style="display:none;" min="1" max="3" value="1"', labeled: false }],];
 
         addFeature(featuresList);
         handleInput(['questionSpoof', 'videoSpoof', 'showAnswers', 'nextRecomendation', 'repeatQuestion', 'minuteFarm', 'customBanner', 'rgbLogo']);
-        handleInput('autoAnswer', checked => {
-            if (checked) {
-                document.querySelectorAll('.autoAnswerDelay, .nextRecomendation, .repeatQuestion').forEach(el => el.style.display = null);
-            } else {
-                document.querySelectorAll('.autoAnswerDelay, .nextRecomendation, .repeatQuestion').forEach(el => el.style.display = 'none');
-            }
-        });
+        handleInput('autoAnswer', checked => checked && !features.questionSpoof && (document.querySelector('[setting-data="features.questionSpoof"]').checked = features.questionSpoof = true));
         handleInput('autoAnswerDelay', value => value && (featureConfigs.autoAnswerDelay = 4 - value));
-
-        // Close button
-        const closeButton = document.createElement('button');
-        closeButton.textContent = 'Close';
-        Object.assign(closeButton.style, {
-            marginTop: '20px', padding: '10px', backgroundColor: '#540b8a', color: 'white',
-            border: 'none', borderRadius: '5px', cursor: 'pointer'
-        });
-        closeButton.addEventListener('click', () => {
-            // Save settings before closing
-            document.querySelectorAll('[setting-data]').forEach(element => {
-                const setting = element.getAttribute('setting-data');
-                if (element.type === 'checkbox') {
-                    setFeatureByPath(setting, element.checked);
-                } else {
-                    setFeatureByPath(setting, element.value);
-                }
-            });
-            optionsWindow.remove();
-        });
-        optionsWindow.appendChild(closeButton);
-
-        document.body.appendChild(optionsWindow);
-    }
-
-    function setupDropdown() {
-        Object.assign(dropdownMenu.style, {
-            display: 'none' // Hide the original dropdown menu
-        });
-        watermark.addEventListener('click', openOptionsWindow);
+        handleInput('darkMode', checked => checked ? (DarkReader.setFetchMethod(window.fetch), DarkReader.enable()) : DarkReader.disable());
+        watermark.addEventListener('mouseenter', () => { dropdownMenu.style.display = 'flex'; playAudio('https://r2.e-z.host/4d0a0bea-60f8-44d6-9e74-3032a64a9f32/3kd01iyj.wav'); } );
+        watermark.addEventListener('mouseleave', e => { !watermark.contains(e.relatedTarget) && (dropdownMenu.style.display = 'none'); playAudio('https://r2.e-z.host/4d0a0bea-60f8-44d6-9e74-3032a64a9f32/rqizlm03.wav'); });
     }
     function setupStatusPanel() {
         Object.assign(statsPanel.style, {
